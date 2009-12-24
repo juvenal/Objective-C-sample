7f2af0d9edc0d5a34552d2a7a0d15cda9d19ee08
Juvenal A. Silva Jr <juvenal.silva.jr@gmail.com>
2009-12-24 20:52:00 -0300

    - Fix in some comments in the Objective-c source file sample.

--- test.m
+++ test.m
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[]) {
 	// MyClass *message = [MyClass new];       // C++ class init format
-	MyClass *message = [[MyClass alloc] init]; // Objective_C class init format
+	MyClass *message = [[MyClass alloc] init]; // Objective-C class init format
     [message displayMsg:"Eita mundo! Oia nois aqui" lines:10];
     return 0;
 }
