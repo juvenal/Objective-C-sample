ecce2a455aa90c9ff0d58a3d9db1d99f21fc5cdc
Juvenal A. Silva Jr <juvenal.silva.jr@gmail.com>
2012-01-05 16:00:22 -0200

    Uncomment the demo for the alternative way of Objective-C instance initialization method

--- test.m
+++ test.m
@@ -11,11 +11,11 @@
 #import "myclass.h"
 
 int main(int argc, char *argv[]) {
-	// MyClass *myclass = [MyClass new];       // C++ class init format
 	MyClass *myclass = [[MyClass alloc] init]; // Objective-C class init format
+	MyClass *myclass2 = [MyClass new];         // C++ class init format (alloc and init together)
 	[myclass setTimes:10];
 	[myclass displayMsg:"Eita mundo! Oia nois aqui"];
-	[myclass displayMsg:"Hello world! Here we are" times: 8];
+	[myclass2 displayMsg:"Hello world! Here we are" times: 8];
 	return 0;
 }
 
