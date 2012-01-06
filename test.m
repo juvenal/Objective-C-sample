55771c75d8a2df8b063e2e812d027cacc6dce52c
Juvenal A. Silva Jr <juvenal.silva.jr@gmail.com>
2012-01-05 23:49:37 -0200

    Changed the numbers on the loops

--- test.m
+++ test.m
@@ -14,11 +14,11 @@ int main(int argc, char *argv[]) {
 	MyClass *myclass = [[MyClass alloc] init]; // Objective-C class init format
 	MyClass *myclass2 = [MyClass new];         // C++ class init format (alloc and init together)
 	NSNumber *timesValue;
-	[myclass setTimes: 10];
+	[myclass setTimes: 5];
 	[myclass displayMsg: "Eita mundo! Oia nois aqui"];
-	timesValue = [NSNumber numberWithInt: 5];
+	timesValue = [NSNumber numberWithInt: 10];
 	[myclass showString: @"Eita mundo! De novo!" times: timesValue];
-	[myclass2 displayMsg: "Hello world! Here we are" times: 8];
+	[myclass2 displayMsg: "Hello world! Here we are" times: 15];
 	return 0;
 }
 
