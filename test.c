a5befd148751da2e7eff14cf6d0006af136bbec7
Juvenal A. Silva Jr <juvenal.silva.jr@gmail.com>
2013-09-24 23:07:02 -0300

    Fix an output typo on test-c output
    Isolate the single and plural outputs depending on the line number
    output being used.

--- test.c
+++ test.c
@@ -14,9 +14,14 @@ int main(int argc, char *argv[]) {
 	int i = 0;
 
 	for (i = 0; i < 10; i++) {
-		printf("Hello World %d time!\n", i + 1);
+		if (i > 0) {
+			printf("Hello World %d times!\n", i + 1);
+		}
+		else {
+			printf("Hello World %d time!\n", i + 1);
+		}
 	}
-	return 0;
+	return EXIT_SUCCESS;
 }
 
 /* vim: set ts=4 ai: */
